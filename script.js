445f47272f1331b2198524b352dc862eaa4d6110
Neosoulink <neodarksoulink@gmail.com>
2021-04-03 10:36:50 +0100

    Shrink effect on scroll + transparence brand

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ window.onload = function (e) {
 	document.body.style.display = "flex";
 
 	const content = document.getElementById('content');
+	const navBrand = document.querySelector('.nav-brand');
 	const navFilter = document.querySelector('.nav-filter');
 
-	content.addEventListener('scroll', () => {
-		stickyNavFilter(content.scrollTop, navFilter)
-	});
-
-	function stickyNavFilter(pageYOffset = window.pageYOffset, elToStick = navFilter) {
+	function stickyElement(pageYOffset = window.pageYOffset, elToStick = Object, shrink = false) {
 		if (pageYOffset >= elToStick.offsetTop) {
 			elToStick.classList.add('sticky');
+			elToStick.classList.add('trans');
+			if (shrink) elToStick.classList.add('shrink');
 		} else {
 			elToStick.classList.remove('sticky');
+			elToStick.classList.remove('trans');
+			if (shrink) elToStick.classList.remove('shrink');
 		}
 	}
+
+	content.addEventListener('scroll', () => {
+		stickyElement(content.scrollTop, navBrand, true);
+
+		stickyElement(content.scrollTop, navFilter);
+	});
+
 }
