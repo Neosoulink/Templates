b59ef0ce6aedeea866f2193c9ba7fcc4561542db
Neosoulink <neodarksoulink@gmail.com>
2021-04-03 14:13:40 +0100

    Custom cursor

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.document.body.style.display = "none";
 window.onload = function (e) {
 	document.body.style.display = "flex";
 
+	const cursor = document.querySelector(".cursor");
 	const content = document.getElementById('content');
 	const navBrand = document.querySelector('.nav-brand');
 	const navFilter = document.querySelector('.nav-filter');
@@ -19,10 +20,23 @@ window.onload = function (e) {
 		}
 	}
 
+	function customizeCursor(e) {
+		cursor.style.top = e.pageY + "px";
+		cursor.style.left = e.pageX + "px";
+		if (['a', 'scrollBar'].includes(e.target.tagName.toLowerCase())) {
+			cursor.classList.add('active');
+		} else {
+			cursor.classList.remove('active');
+		}
+	}
+
 	content.addEventListener('scroll', () => {
 		stickyElement(content.scrollTop, navBrand, true);
-
 		stickyElement(content.scrollTop, navFilter);
 	});
 
+	document.addEventListener("mousemove", customizeCursor);
+
+	document.addEventListener("mousedown", () => cursor.classList.add('active'));
+	document.addEventListener("mouseup", () => cursor.classList.remove('active'));
 }
