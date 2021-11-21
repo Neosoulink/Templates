daedef1136f3d70fa460bdb6bbec6a2f13ae53b3
Neosoulink <neodarksoulink@gmail.com>
2021-11-21 14:48:10 +0100

    Remove none pro templates

--- minhealth/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    document.querySelector('.navbar.fixed-top').style.backgroundColor = "rgba(0,0,0,0.8)";
-  } else {
-    document.querySelector('.navbar.fixed-top').style.backgroundColor = "rgba(100,100,100,0.3)";
-  }
-}
