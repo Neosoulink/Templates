12721801c8f2d101bb96df01eabb5b5998a1fe32
Neosoulink <neodarksoulink@gmail.com>
2021-11-22 00:23:06 +0100

    Minor fix

--- assets/js/helpers/Functions.ts
+++ assets/js/helpers/Functions.ts
@@ -1,10 +1,10 @@
-import { aNodes, cursor } from "./HtmlDomElementsHelper";
+import { cursor } from "./HtmlDomElementsHelper";
 
 export function stickyElement(
-	pageYOffset = window.pageYOffset,
-	elToStick = Object,
-	shrink = false
-) {
+	pageYOffset: number = window.pageYOffset,
+	elToStick: HTMLDivElement,
+	shrink: boolean = false
+): void {
 	if (pageYOffset > elToStick.offsetTop) {
 		elToStick.classList.add("sticky");
 		elToStick.classList.add("trans");
@@ -16,17 +16,17 @@ export function stickyElement(
 	}
 }
 
-export function customizeCursorMovement(e) {
+export function customizeCursorMovement(e: MouseEvent): void {
 	cursor.style.top = e.pageY + "px";
 	cursor.style.left = e.pageX + "px";
 }
 
-export function activeCursor({ def = true, lg = false }) {
+export function activeCursor({ def = true, lg = false }): void {
 	if (def) cursor.classList.add("active");
 	if (lg) cursor.classList.add("active-lg");
 }
 
-export function disableCursor() {
+export function disableCursor(): void {
 	cursor.classList.remove("active");
 	cursor.classList.remove("active-lg");
 }
